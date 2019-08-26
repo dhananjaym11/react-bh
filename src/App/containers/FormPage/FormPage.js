8e88f72108e3eb244372dc95008e5767b0696ffc
Dhananjay Mane <dhananjay.mane@xoriant.com>
2019-08-26 11:15:46 +0530

    Added home & form components

---
+++ src/App/containers/FormPage/FormPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+
+export default function FormPage() {
+    return (
+        <Container>
+            <h1>Form Page</h1>
+        </Container>
+    )
+}
